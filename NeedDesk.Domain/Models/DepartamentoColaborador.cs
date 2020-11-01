485e19ed88515fc093e66bdf144ce9efe9c5269b
André Oliveira <inforoliver@gmail.com>
2020-11-01 09:34:22 -0300

    New model

---
+++ NeedDesk.Domain/Models/DepartamentoColaborador.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedDesk.Domain.Models
+{
+    [Table("Departamentos_Colaboradores")]
+    public class DepartamentoColaborador : EntityBase
+    {
+        public Int64 Tenant_id { get; set; }
+        [Key]
+        public Int64 Dec_id { get; set; }
+        public Int64 Dep_id { get; set; }
+        public Int64 Col_id { get; set; }
+    }
+}
---
+++ NeedDesk.Infra.Data.Tests/DepartamentoColaboradoreRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using NeedDesk.Domain.Interfaces.Repositories;
+using NeedDesk.Domain.Models;
+using NeedDesk.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NeedDesk.Infra.Data.Tests
+{
+    public class DepartamentoColaboradoreRepositoryTests
+    {
+        [Fact]
+        public void CrudTest()
+        {
+            try
+            {
+                IDepartamentoColaboradoreRepository departamentoColaboradoreRepository = new DepartamentoColaboradoreRepository();
+
+                // test insert
+                var id = (Int64)departamentoColaboradoreRepository.Insert(NewDepartamentoColaborador());
+                Assert.True(id > 0);
+
+                // test get by id
+                DepartamentoColaborador departamentoColaborador = departamentoColaboradoreRepository.FindById(id);
+                Assert.True(departamentoColaborador != null);
+
+                // test update
+                departamentoColaborador.CreateAt = DateTime.Now;
+                var rowscuccess = departamentoColaboradoreRepository.Update(departamentoColaborador);
+                Assert.True(rowscuccess > 0);
+
+                // test delete
+                departamentoColaboradoreRepository.Delete(departamentoColaborador);
+
+                for (int i = 1; i <= 5; i++)
+                    departamentoColaboradoreRepository.Insert(NewDepartamentoColaborador());
+
+                var list = departamentoColaboradoreRepository.All("tenant_id > 0");
+                Assert.True(list.Count() > 0);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private DepartamentoColaborador NewDepartamentoColaborador()
+        {
+            return new DepartamentoColaborador()
+            {
+                Tenant_id = CreateTenant.Tenant_id(),
+                Dep_id = CreateDepartamento.Departamento().Dep_id,
+                Col_id = CreateColaborador.Colaborador().Col_id
+            };
+        }
+    }
+}
